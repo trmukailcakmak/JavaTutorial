a07e9ce0de2dcec0301b687b5093c15be702c547
Mükail Çakmak <tr.mukailcakmak@gmail.com>
2021-10-07 22:07:03 +0300

    Revert "[MukailCakmak] bosluk test amaclı"
    This reverts commit aa13dfd6bb32a2d068994aef4918fc8e2ee97156.

--- src/com/cakmak/ders/main/Main.java
+++ src/com/cakmak/ders/main/Main.java
@@ -19,8 +19,6 @@ public class Main {
         System.out.print("yükseklik giriniz : ");
         yukseklik = Giris.nextInt();
         Main2.yildizBoslukCizX(yukseklik);
-
-
     }
        /* System.out.println("Main call edıldi.");
         yildizCiz(10);
