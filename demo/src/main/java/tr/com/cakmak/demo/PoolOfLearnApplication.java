65f31a055e8c63d282334352151c34592afe79ec
Halil Çakmak <halil.cakmak@consultant.turkcell.com.tr>
2021-10-10 23:15:53 +0300

    [HalilCakmak] demo projesi eklendi

---
+++ demo/src/main/java/tr/com/cakmak/demo/PoolOfLearnApplication.java
@@ -0,0 +1,13 @@
+package tr.com.cakmak.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolOfLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolOfLearnApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/tr/com/cakmak/demo/PoolOfLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.cakmak.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolOfLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
