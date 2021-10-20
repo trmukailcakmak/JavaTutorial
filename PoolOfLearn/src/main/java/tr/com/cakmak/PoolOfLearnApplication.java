0ae499207c076fd2d6b07d9eeaab4fa2742c788f
Mükail Çakmak <tr.mukailcakmak@gmail.com>
2021-10-20 09:15:14 +0300

    Database baglantısı basarıyla yapıldı.

--- PoolOfLearn/src/main/java/tr/com/cakmak/PoolOfLearnApplication.java
+++ PoolOfLearn/src/main/java/tr/com/cakmak/PoolOfLearnApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PoolOfLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PoolOfLearnApplication.class, args);
+
+		System.out.println("Hello world!");
 	}
 
 }
